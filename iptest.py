435ac6a4ace6b22552dfa970363b81f108327047
vpnineh <fffarbod@bk.ru>
2024-09-22 03:53:45 +0330

    Update iptest.py

--- iptest.py
+++ iptest.py
@@ -1,26 +1,27 @@
 import base64
 import json
+
 import urllib.request 
 urllib.request.urlretrieve("https://raw.githubusercontent.com/Surfboardv2ray/Subs/refs/heads/main/IPTest/ips.txt", "ip.txt")
-urllib.request.urlretrieve("https://raw.githubusercontent.com/Surfboardv2ray/Subs/refs/heads/main/IPTest/bestips.txt", "bestips.txt")
+
 
 # Open the source text files
 file1 = open('ip.txt', 'r')
-file2 = open('bestips.txt', 'r')
+
 
 # Read the contents of the text files
 content1 = file1.read()
-content2 = file2.read()
+
 
 # Close the source text files
 file1.close()
-file2.close()
+
 
 # Open the destination file
 destination_file = open('ips.txt', 'w')
 
 # Write the concatenated content to the destination file
-destination_file.write(content1 + content2)
+destination_file.write(content1)
 # Close the destination file
 destination_file.close()
 
