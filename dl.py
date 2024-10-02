fe89ec5980d1ae7113f47c0b62fe8db6646a44ce
vpnineh <fffarbod@bk.ru>
2024-10-02 19:13:58 +0330

    Update dl.py

--- dl.py
+++ dl.py
@@ -1,2 +1,2 @@
 import urllib.request 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/Surfboardv2ray/Subs/refs/heads/main/IPTest/ips.txt", "ips.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/Surfboardv2ray/Subs/refs/heads/main/IPTest/bestips.txt", "ips.txt")
